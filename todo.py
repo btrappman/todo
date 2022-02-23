f518ad4df99c5b9d0098ff20c78f2ada6d0e8ce8
Brandon <btrappman@protonmail.com>
2022-02-23 15:45:05 -0700

    Exception handling

--- todo.py
+++ todo.py
@@ -3,30 +3,34 @@ import datetime
 
 def main():
     # Incorrect nunmber of arguments
-    if len(sys.argv) == 1:
-        help()
+    #if len(sys.argv) == 1:
+        #help()
+    try:
+        # User command
+        command = sys.argv[1]
 
-    # User command
-    command = sys.argv[1]
-
-    # Add new todo item
-    if command == "add" and len(sys.argv) != 3:
-        help()
-    elif command == "add":
-        newTodo = sys.argv[2]
-        add(newTodo)
+        # Add new todo item
+        if command == "add" and len(sys.argv) != 3:
+            help()
+        elif command == "add":
+            newTodo = sys.argv[2]
+            add(newTodo)
+            
+        # Print help menu
+        if command == "help":
+            help()
         
-    # Print help menu
-    if command == "help":
-        help()
-    
-    # Check successful add()
-    with open("todo.txt", "r") as file:
-        print(file.read())
+        # Check successful add()
+        with open("todo.txt", "r") as file:
+            print(file.read())
 
-    # Test successful run
-    print("success")
-    sys.exit(0)
+        # Test successful run
+        print("success")
+        sys.exit(0)
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        help()
 
 # Add new todo item
 def add(newItem):
@@ -35,6 +39,14 @@ def add(newItem):
         file.write("\n")
     newItem = '"' + newItem + '"'
     print(f"Successfully added: {newItem}")
+
+# Loads todo list into dictionary
+# TODO: 
+def load():
+    with open("todo.txt", "r") as file:
+        itemNum = 1
+        for line in file:
+            line = line.strip("\n")
 
 
 # Displays usage
