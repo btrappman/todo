11cfd46e329e06f75efba7ca150dec769733cf9f
Brandon <btrappman@protonmail.com>
2022-02-23 13:07:16 -0700

    Added help()

--- todo.py
+++ todo.py
@@ -1,1 +1,27 @@
-print("Hello world")
+import sys
+import datetime
+
+def main():
+    # Incorrect nunmber of arguments
+    if len(sys.argv) == 1:
+        help()
+        sys.exit(1)
+
+    # Help 
+    command = sys.argv[1]
+    if command == "help":
+        help()
+
+    print("success")
+
+# Displays usage
+def help():
+    # Triple quotes allow strings that span multiple lines
+    help = """Usage:
+    $ ./todo help       # Usage"""
+
+    print(help)
+
+# Runs main
+if __name__ == "__main__":
+    main()
